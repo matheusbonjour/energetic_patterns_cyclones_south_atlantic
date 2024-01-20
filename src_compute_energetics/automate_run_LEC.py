f5964897e0a9f248ac78deb62ac63e5c5534d6ba
daniloceano <danilo.oceano@gmail.com>
2024-01-20 13:47:33 -0300

    improved logging and testing

--- src_compute_energetics/automate_run_LEC.py
+++ src_compute_energetics/automate_run_LEC.py
@@ -3,23 +3,43 @@ import os
 import subprocess
 import time
 from tqdm import tqdm
+import logging
+
+# Logging configuration
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s', 
+                    filename='log.automate_run_LEC.txt', 
+                    filemode='w')  # 'w' for overwrite, 'a' for append
 
 FILTERED_TRACKS = '../tracks_SAt_filtered/tracks_SAt_filtered.csv'
 REGION = 'SE-BR'
-LEC_PATH = '../lorenz-cycle/lorenz_cycle.py'
+LEC_PATH = '../../lorenz-cycle/lorenz_cycle.py'
 
 tracks = pd.read_csv(FILTERED_TRACKS)
 tracks_region = tracks[tracks['region'] == REGION]
 system_ids = tracks_region['track_id'].unique()
 
-# Limit to the first 10 cases for testing
+# Limit to the first 3 cases for testing
 system_ids = system_ids[:3]
 
 # Store execution times
 execution_times = []
 
-# Change directory to the program directory
-os.chdir(os.path.dirname(LEC_PATH))
+try:
+    # Change directory to the program directory
+    os.chdir(os.path.dirname(LEC_PATH))
+    logging.info(f"Changed directory to {os.path.dirname(LEC_PATH)}")
+except Exception as e:
+    logging.error(f"Error changing directory: {e}")
+    exit(1)
+
+try:
+    # Pull the latest changes from Git
+    subprocess.run(["git", "pull"])
+    logging.info("Successfully pulled latest changes from Git")
+except Exception as e:
+    logging.error(f"Error pulling latest changes from Git: {e}")
+    exit(1)
 
 # Process each system ID
 for id in tqdm(system_ids):
@@ -27,7 +47,12 @@ for id in tqdm(system_ids):
     command = ['python', LEC_PATH] + arguments
 
     start_time = time.time()
-    subprocess.run(command)
+    try:
+        subprocess.run(command)
+        logging.info(f"Successfully ran Lorenz Cycle script for ID {id}")
+    except Exception as e:
+        logging.error(f"Error running Lorenz Cycle script for ID {id}: {e}")
+        continue
     end_time = time.time()
 
     # Record the time taken for this execution
@@ -37,5 +62,5 @@ for id in tqdm(system_ids):
 total_time = sum(execution_times)
 mean_time = total_time / len(execution_times)
 
-print(f'Total time for {len(system_ids)} cases: {total_time:.2f} seconds')
-print(f'Mean time per case: {mean_time:.2f} seconds')
+logging.info(f'Total time for {len(system_ids)} cases: {total_time:.2f} seconds')
+logging.info(f'Mean time per case: {mean_time:.2f} seconds')
