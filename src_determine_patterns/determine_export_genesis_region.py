e3bad719582235101b229716b88199319e4ff40c
daniloceano <danilo.oceano@gmail.com>
2024-03-01 16:14:59 -0300

    analysing for each region

--- src_determine_patterns/determine_export_genesis_region.py
+++ src_determine_patterns/determine_export_genesis_region.py
@@ -1,34 +1,36 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    determine_export_genesis_region.py                 :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: daniloceano <danilo.oceano@gmail.com>      +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/03/01 15:05:41 by daniloceano       #+#    #+#              #
-#    Updated: 2024/03/01 15:09:37 by daniloceano      ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-"""
-Export csv files containing track ids of systems with genesis on each region.
-"""
-
 import os
 import pandas as pd
+from glob import glob
 
 output_path = "../csv_life_cycle_analysis/"
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
 tracks_database_path = "../tracks_SAt_filtered/"
 tracks = pd.read_csv(f"{tracks_database_path}/tracks_SAt_filtered.csv")
 
+processed_systems_path = "../csv_database_energy_by_periods/"
+processed_systems_files = glob(f"{processed_systems_path}/*_averages.csv")
+
+# Extract system IDs from the processed files' names
+processed_systems_ids = [os.path.basename(f).split('_')[0] for f in processed_systems_files]
+
 genesis_regions = tracks['region'].unique()
+region_summary = []
 
 for region in genesis_regions:
-
     filtered_tracks = tracks[tracks['region'] == region]
-    systems = list(filtered_tracks['track_id'].unique())
+    systems = list(filtered_tracks['track_id'].astype(str).unique())
 
-    filename = f"track_ids_{region}.txt"
-    with open(f"{os.path.join(output_path, filename)}", "w") as f:
-        for system in systems:
-            f.write(f"{system}\n")
+    # Count how many systems in this region have been processed successfully
+    successful_cases = len([system for system in systems if system in processed_systems_ids])
+    
+    region_summary.append([region, len(systems), successful_cases])
+
+# Convert summary list to DataFrame
+df_summary = pd.DataFrame(region_summary, columns=['Region', 'Total Systems', 'Successful Cases'])
+
+# Save to CSV
+summary_csv_path = os.path.join(output_path, "genesis_region_summary.csv")
+df_summary.to_csv(summary_csv_path, index=False)
+print(f"Summary saved to {summary_csv_path}")
